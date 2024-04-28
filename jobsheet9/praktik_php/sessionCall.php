3e831473edbb25c2553482573fc9427cc2c7c237
anaradiocta <nelaocta12@gmail.com>
2024-04-28 16:30:52 +0700

    Jobsheet 9

---
+++ jobsheet9/praktik_php/sessionCall.php
@@ -0,0 +1,12 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            echo "Favorite Color Is : " . $_SESSION["favcolor"] . ". <br>";
+            echo "Favorite Animal Is : " . $_SESSION["favanimal"] . "";
+        ?>
+    </body>
+</html>
