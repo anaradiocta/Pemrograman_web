2e78abcbbd04d4a84e54fcd3e8d66155e068eb29
anaradiocta <nelaocta12@gmail.com>
2024-05-02 20:58:49 +0700

    Jobsheet 11

---
+++ jobsheet11/index.php
@@ -0,0 +1,20 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (!empty($_SESSION['level'])) {
+    require 'config/koneksi.php';
+    require 'fungsi/pesan_kilat.php';
+    
+
+    include 'admin/header.php';
+    if (!empty($_GET['page'])) {
+        include 'admin/module/' . $_GET['page'] . '/index.php';
+    } else {
+        include 'template/home.php';
+    }
+    include 'template/footer.php';
+} else {
+    header("Location: login.php");
+}
